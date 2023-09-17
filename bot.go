8c4cc720a216d389625c08bf3375151eab5f3270
dbzyuzin <dbzyuzin@gmail.com>
2023-09-17 07:10:56 -0400

    add message deletion

--- bot.go
+++ bot.go
@@ -2,6 +2,8 @@ package tgbot
 
 import (
 	"fmt"
+	"strconv"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
@@ -11,6 +13,16 @@ func SendMessage(chatID int64, text string) {
 	_, err := bot.Send(msg)
 	if err != nil {
 		fmt.Println("Не удалось отправить сообщение в чат", chatID)
+	}
+}
+
+func DeleteMessage(chatID int64, message_id int) {
+	_, err := bot.MakeRequest("deleteMessage", map[string]string{
+		"chat_id":    strconv.FormatInt(chatID, 10),
+		"message_id": strconv.FormatInt(int64(message_id), 10),
+	})
+	if err != nil {
+		fmt.Printf("Не удалось удалить сообщение %d в чатe %d", message_id, chatID)
 	}
 }
 
