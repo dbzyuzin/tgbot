f89ba577457435de59855f4e7616a576d8806ba7
dbzyuzin <dbzyuzin@gmail.com>
2025-03-15 00:13:24 +0400

    add buttons to all messages

--- bot.go
+++ bot.go
@@ -2,23 +2,46 @@ package tgbot
 
 import (
 	"fmt"
-	"strconv"
-
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"strconv"
 )
 
-func SendMessage(chatID int64, text string) {
+func SendMessage(chatID int64, text string, buttons ...[]Button) {
 	msg := tgbotapi.NewMessage(chatID, text)
+
+	addKeyboard(&msg, buttons)
+
+	_, err := bot.Send(msg)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Не удалось отправить сообщение в чат", chatID)
+	}
+}
+
+func ReplyMessage(chatID int64, msgID int, text string, buttons ...[]Button) {
+	msg := tgbotapi.NewMessage(chatID, text)
+	msg.ReplyToMessageID = msgID
+
+	addKeyboard(&msg, buttons)
+
 	_, err := bot.Send(msg)
 	if err != nil {
 		fmt.Println("Не удалось отправить сообщение в чат", chatID)
 	}
 }
 
-func SendMessageWithKeyboard(chatID int64, text string, buttons [][]Button) {
-	msg := tgbotapi.NewMessage(chatID, text)
+func DeleteMessage(chatID int64, messageID int) {
+	_, err := bot.MakeRequest("deleteMessage", map[string]string{
+		"chat_id":    strconv.FormatInt(chatID, 10),
+		"message_id": strconv.FormatInt(int64(messageID), 10),
+	})
+	if err != nil {
+		fmt.Printf("Не удалось удалить сообщение %d в чатe %d", messageID, chatID)
+	}
+}
 
-	markupTable := [][]tgbotapi.InlineKeyboardButton{}
+func addKeyboard(msg *tgbotapi.MessageConfig, buttons [][]Button) {
+	var markupTable [][]tgbotapi.InlineKeyboardButton
 	for _, row := range buttons {
 		r := make([]tgbotapi.InlineKeyboardButton, 0, len(row))
 		for _, btn := range row {
@@ -28,31 +51,5 @@ func SendMessageWithKeyboard(chatID int64, text string, buttons [][]Button) {
 	}
 	if len(buttons) != 0 && len(buttons[0]) != 0 {
 		msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(markupTable...)
-	}
-
-	_, err := bot.Send(msg)
-	if err != nil {
-		fmt.Println(err)
-		fmt.Println("Не удалось отправить сообщение в чат", chatID)
-	}
-}
-
-func DeleteMessage(chatID int64, message_id int) {
-	_, err := bot.MakeRequest("deleteMessage", map[string]string{
-		"chat_id":    strconv.FormatInt(chatID, 10),
-		"message_id": strconv.FormatInt(int64(message_id), 10),
-	})
-	if err != nil {
-		fmt.Printf("Не удалось удалить сообщение %d в чатe %d", message_id, chatID)
-	}
-}
-
-func ReplyMessage(chatID int64, msgID int, text string) {
-	msg := tgbotapi.NewMessage(chatID, text)
-	msg.ReplyToMessageID = msgID
-
-	_, err := bot.Send(msg)
-	if err != nil {
-		fmt.Println("Не удалось отправить сообщение в чат", chatID)
 	}
 }
