7d91c3ba17b16b9573f871f66050255efce2dca0
dbzyuzin <dbzyuzin@gmail.com>
2025-05-17 17:24:23 +0400

    add method to edit inline keyboard

--- bot.go
+++ bot.go
@@ -10,7 +10,7 @@ import (
 
 func SendMessage(chatID int64, text string, buttons ...[]Button) {
 	msg := tu.Message(tu.ID(chatID), text)
-	addKeyboard(msg, buttons)
+	msg.ReplyMarkup = createInlineKeyboard(buttons)
 
 	_, err := bot.SendMessage(context.Background(), msg)
 	if err != nil {
@@ -20,7 +20,7 @@ func SendMessage(chatID int64, text string, buttons ...[]Button) {
 
 func ReplyMessage(chatID int64, msgID int, text string, buttons ...[]Button) {
 	msg := tu.Message(tu.ID(chatID), text)
-	addKeyboard(msg, buttons)
+	msg.ReplyMarkup = createInlineKeyboard(buttons)
 
 	msg.ReplyParameters = &telego.ReplyParameters{
 		MessageID: msgID,
@@ -38,7 +38,20 @@ func DeleteMessage(chatID int64, messageID int) {
 	}
 }
 
-func addKeyboard(msg *telego.SendMessageParams, buttons [][]Button) {
+func UpdateKeyboard(chatID int64, messageID int, buttons ...[]Button) {
+	keyboard := createInlineKeyboard(buttons)
+
+	_, err := bot.EditMessageReplyMarkup(context.Background(), &telego.EditMessageReplyMarkupParams{
+		ChatID:      tu.ID(chatID),
+		MessageID:   messageID,
+		ReplyMarkup: keyboard,
+	})
+	if err != nil {
+		slog.Error("can't edit the keyboard", "chat", chatID, "msg_id", messageID, "err", err)
+	}
+}
+
+func createInlineKeyboard(buttons [][]Button) *telego.InlineKeyboardMarkup {
 	var markupTable [][]telego.InlineKeyboardButton
 	for _, row := range buttons {
 		r := make([]telego.InlineKeyboardButton, 0, len(row))
@@ -49,8 +62,8 @@ func addKeyboard(msg *telego.SendMessageParams, buttons [][]Button) {
 	}
 
 	if len(buttons) != 0 && len(buttons[0]) != 0 {
-		msg.ReplyMarkup = tu.InlineKeyboard(
-			markupTable...,
-		)
+		return tu.InlineKeyboard(markupTable...)
 	}
+
+	return nil
 }
